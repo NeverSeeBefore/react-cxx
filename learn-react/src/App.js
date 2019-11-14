ae8d7176a66026f4585ef3021911329dc3c5e45d
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-11-14 17:27:51 +0800

    'init'

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import PageTest from './components/pageTest/PageTest';
 
 function App() {
   return (
     <div className="App">
       Learn React
+      <PageTest></PageTest>
     </div>
   );
 }
