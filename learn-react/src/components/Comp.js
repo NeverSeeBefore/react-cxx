120d8cbb48838c3a4f5162cf1f5a97fb4aee7276
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-11-22 11:24:01 +0800

    'update'

--- learn-react/src/components/Comp.js
+++ learn-react/src/components/Comp.js
@@ -8,21 +8,9 @@ class Comp extends Component {
     console.log("render ", this.state.n)
     return (
       <div>
-        <h1>{this.state.n}
-        </h1>
-        <div onClick={this.handleAdd}>+</div>
+        
       </div>
     )
-  }
-  handleAdd = () => {
-    this.setState({
-      n: this.state.n + 1
-    }, () => {
-      console.log("handleAdd -> setState", this.state.n)
-    })
-    console.log("handleAdd ", this.state.n)
-    // render (new) ----- handleAdd n(old) ----- handleAdd -> setState n(new)
-
   }
 
 }
--- learn-react/src/components/Compcopy.js
+++ learn-react/src/components/Compcopy.js
@@ -5,39 +5,12 @@ class Comp extends Component {
     n: 0
   }
   render () {
-    console.log("render ", this.state.n)
+
     return (
       <div>
-        <h1>{this.state.n}
-        </h1>
-        <div onClick={this.handleAdd}>+</div>
+        
       </div>
     )
-  }
-  handleAdd = () => {
-    this.setState(prev => {
-      // prev 表示之前状态
-      // 给函数的返回结果，会混合(覆盖)掉之前的状态
-      // 这个函数不会立即执行，会在适当的时间执行
-      console.log("handleAdd -> setState", prev.n)
-      return {
-        n : prev.n + 1
-      }
-      // 而且参数时可以信任的。
-    })
-    this.setState(prev => {
-      console.log("handleAdd -> setState", prev.n)
-      return {
-        n : prev.n + 1
-      }
-    })
-    this.setState(prev => {
-      console.log("handleAdd -> setState", prev.n)
-      return {
-        n : prev.n + 1
-      }
-    })
-    console.log("handleAdd ", this.state.n)
   }
 
 }
