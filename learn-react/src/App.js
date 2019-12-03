595c8c19cbf0979a10d148a2a5861881529f6f84
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-12-03 14:56:53 +0800

    merge HOC

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,15 +1,6 @@
 import React from "react";
 import "./App.css";
-import {A, B} from './components/Comp.js';
-import withLog from './HOC/withLog.js';
-import withLogin from './HOC/widthLogin.js';
 
-const ALog = withLog(A);
-const BLog = withLog(B);
-const ALogin = withLogin(A);
-let ALogLogin = withLog(A);
-ALogLogin = withLogin(ALogLogin);
-const BLogLogin = withLogin(withLog(B));
 class App extends React.Component {
   state = {
     
@@ -21,12 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <ALog a='11111'/>
-        <BLog b='22222'/>
-        <ALogin a='nologin' />
-        <ALogin a='loginnnn' isLogin />
-        <ALogLogin isLogin a='log + login'></ALogLogin>
-        <BLogLogin isLogin b='login(log(B))' />
+        
       </div>
     );
   }
