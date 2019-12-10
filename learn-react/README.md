fbeafceacb170e64f767a77f621779a7c5dbb75e
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-12-10 15:46:18 +0800

    'init'

--- learn-react/README.md
+++ learn-react/README.md
@@ -1,15 +1,20 @@
-**高阶组件**
-HOC： Higher-Order Component 高阶组件，以组件作为参数，并返回一个组件
+# react 中的事件
 
-组件： function comp () {
-  return <h1>这是组件</h1>
-}
+react内置的dom组件中的事件
 
-react组件元素： <comp />
-react html element:<h1></h1>
+1. 给document注册时间
+2. **几乎**所有元素的事件处理都在document的事件中处理,**focus**事件是不冒泡的，所以根本不可能用document代替
+3. 在document的事件处理，react会根据虚拟dom树，依次完成事件的处理
+4. React的事件参数，不是真实的dom事件参数，是react合成的对象;
 
-通产，可以利用HOC实现横切关注点
+如果给真实的dom元素阻止冒泡，会导致react事件不发触发；
+给真实dom注册事件，会先于react的事件触发
+通过react注册冒泡不能阻止真实dom注册的事件
 
-> 举例
-> 20个组件在创建组件和销毁组件时，需要做日志记录
-> 20个组件，他们需要显示一些内容，得到的数据结构完全一致
+通过react的参数获取真是dom的事件参数  **e.nativeEvent**
+**e.stopImmediatePropagation**  阻止document上剩余事件的执行;
+
+
+**react事件对象是被重用的**
+如果在事件处理程序中，不要异步的使用事件对象
+如果一定要异步使用，需要调用  **e.persist();**  持久化这个事件对象；
--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -9,10 +9,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Test></Test>
+        {/* <Test></Test> */}
+        <button onClick={(e) => {
+          console.log('按钮被点击')
+
+          e.stopPropagation();  // 阻止react的事件冒泡
+
+          console.log(e.nativeEvent); // 原生的事件参数
+
+          console.log(e.nativeEvent.stopImmediatePropagation()); // 阻止剩余的事件处理程序运行；
+
+          e.persist();  // 持久化事件对象，以允许异步操作
+          setTimeout(() => {
+            console.log(e.type)
+          }, 0)
+
+          
+        }}>button</button>
       </div>
     );
   }
 }
+document.querySelector('#root').onclick=function (e) {
+  // console.log(e);
+  console.log('id为root的div被点击了');
+  // e.stopPropagation();
+}
+
 
 export default App;
