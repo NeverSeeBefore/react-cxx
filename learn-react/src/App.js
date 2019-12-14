f6100c8cfa441a82ca9e0594a8ad244c57a5842d
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-12-14 14:59:21 +0800

    'init'

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const arr = useState(0); // 使用一个状态,状态的默认值为0;
-  // 函数的返回值是一个数组,该数组一定包含两项
-  const n = arr[0]; // 1. 第一项是状态的值
-  const setN = arr[1]; // 2. 改变状态的方法
-  // const [n, setN] = useState(0) // 常用写法
+  const [n, setN] = useState(0);
+  // document.title = `计数器${n}`
+  useEffect(() => {
+    document.title = `计数器${n}`;
+  });
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setN(n => n - 1);
-          setN(n => n - 1);
-        }}
-      >
-        -
-      </button>
       <span>{n}</span>
       <button
         onClick={() => {
@@ -25,7 +17,35 @@ function App() {
       >
         +
       </button>
+      <div>
+        <span>可移动的块</span>
+        <div>
+          <MovableBlock></MovableBlock>
+        </div>
+      </div>
     </div>
   );
 }
 export default App;
+
+function MovableBlock(props) {
+  useEffect(() => {
+    console.log('副作用函数')
+
+    return () => {
+      console.log('清理函数');
+    }
+  });
+  return (
+    <div
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: "orange",
+        position: "fixed",
+        top: "0",
+        left: "0"
+      }}
+    ></div>
+  );
+}
