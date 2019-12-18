e821c820898d82824a9f4224e58214a7ff06a984
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-12-18 10:27:44 +0800

    'init'

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,75 +1,52 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import "./App.css";
 import useReducer from "./components/myHook/useReducer";
 
-/**
- * 根据当前的数据， 生成一个新的数据
- * @param {*} state
- * @param {*} action
- */
-function reducer(state, action) {
-  switch (action.type) {
-    case "increase":
-      return state + 1;
-    case "decrease":
-      if (state === 0) {
-        return 0;
-      }
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-// function App() {
-//   const [n, setN] = useState(0);
-
-//   function dispatch(action) {
-//     const newN = reducer(n, action);
-//     setN(newN);
-//   }
-//   return (
-//     <div className="App">
-//       <button
-//         onClick={() => {
-//           dispatch({ type: "increase" });
-//         }}
-//       >
-//         ++++++
-//       </button>
-//       <span>{n}</span>
-//       <button
-//         onClick={() => {
-//           dispatch({ type: "decrease" });
-//         }}
-//       >
-//         --------
-//       </button>
-//     </div>
-//   );
-// }
-
 function App() {
-  const [n, dispatchN] = useReducer(reducer, 0);
-
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          dispatchN({ type: "increase" });
-        }}
-      >
-        ++++++
-      </button>
-      <span>{n}</span>
-      <button
-        onClick={() => {
-          dispatchN({ type: "decrease" });
-        }}
-      >
-        --------
-      </button>
+    <div>
+      <CompCtx></CompCtx>
     </div>
   );
 }
+
 export default App;
+
+const ctx = React.createContext();
+
+function CompCtx() {
+  return (
+    <div>
+      <ctx.Provider value={{ a: 123 }}>
+        Consumer
+        <ChildCtx></ChildCtx>
+        HOOK
+        <ChildCtxHook></ChildCtxHook>
+      </ctx.Provider>
+    </div>
+  );
+}
+
+// Consumer///////////////
+function ChildCtx() {
+  return (
+    <h1>
+      上下文中的a:
+      <ctx.Consumer>
+        {context => {
+          return context.a;
+        }}
+      </ctx.Consumer>
+    </h1>
+  );
+}
+
+// HOOK///////////////
+function ChildCtxHook(){
+  const context = useContext(ctx);
+  return (
+    <h1>
+      上下文中的a:{context.a}
+    </h1>
+  );
+}
