a9f1174d926841e78ffa6d03fbf22fcff87e5587
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2019-12-30 10:58:36 +0800

    'init'

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,40 +1,12 @@
 import React from "react";
 import "./App.css";
-import { Route, NavLink, BrowserRouter } from "react-router-dom";
-import BeforeRouteLeave from "./BeforeRouteLeave";
-import RootRouter from "./RootRouter";
-
+import "./react-router/pathMatch"
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter getUserConfirmation={(message, cb) => {
-        
-      }}>
-        <div className="nav">
-          <NavLink to="/page1">page1</NavLink>
-          <NavLink to="/page2">page2</NavLink>
-        </div>
-        <Route path="/page1" component={Page1}></Route>
-        <Route path="/page2" component={Page2}></Route>
-      </BrowserRouter>
+      null
     );
   }
 }
 
 export default App;
-
-function Page1(Props) {
-  return (
-    <>
-      <BeforeRouteLeave
-        when={true}
-        message={"确认离开本页面吗"}
-      ></BeforeRouteLeave>
-      <div>page1</div>
-    </>
-  );
-}
-
-function Page2(Props) {
-  return <div>page2</div>;
-}
---
+++ learn-react/src/react-router/pathMatch.js
@@ -0,0 +1,79 @@
+import { pathToRegexp } from "path-to-regexp";
+// const keys = [];
+// const result = pathToRegexp("/news/:id/:page/:class(\\d+)?", keys);
+// console.log(result)
+// console.log(keys)
+// /^\/news(?:\/([^\/#\?]+?))(?:\/([^\/#\?]+?))[\/#\?]?$/i
+// console.log(result.exec('/news/123/ae/1'));
+// console.log(result);
+/**
+ * 得到匹配结果 match对象
+ * 不匹配返回undefined
+ * @param {String} path 路径规则
+ * @param {*} pathname 具体地址
+ * @param {*} options 相关配置{exact, sensitive, strict}
+ */
+export default function matchPath(path, pathname, options) {
+  const keys = [];
+  const regexp = pathToRegexp(path, keys, getOptions(options));
+
+  const result = regexp.exec(pathname);
+  if (!result) return undefined;
+
+  const execArr = Array.from(result.slice(1));
+  const params = getParams(execArr, keys);
+
+  console.log("regexp", regexp);
+  console.log("result", result);
+  console.log("keys", keys);
+  console.log("execArr", execArr);
+  console.log("params", params);
+  return {
+    path,
+    params,
+    url: result[0],
+    isExact: pathname === result[0]
+
+  }
+  // return result;
+}
+
+/**
+ * 根据匹配结果，得到params对象
+ * @param {*} group
+ * @param {*} keys
+ */
+function getParams(group, keys) {
+  const obj = {};
+  for (let i = 0; i < group.length; i++) {
+    const value = group[i];
+    const name = keys[i].name;
+    obj[name] = value;
+  }
+  return obj;
+}
+
+/**
+ * 将传入额router配置，转换为payh-to-regexp配置
+ * @param {*} option 
+ */
+function getOptions(options) {
+  const defaultOptions = {
+    exact: false,
+    sensitive: false,
+    strict: false
+  }
+  const opts = {...defaultOptions, ...options};
+  return {
+    sensitive: opts.sensitive, // 区分大小写
+    strict: opts.strict,  // 严格匹配
+    end: opts.exact       // 匹配到结尾
+  }
+}
+let last = matchPath("/news/:page/:id", "/news/wer/123", { sensitive: true });
+console.log('last',last);
+console.log("----------------------------------------");
+last = matchPath("/news", "/news/dase/ewewa");
+// matchPath("/news", "/news/dase/ewewa", {exact: true});
+console.log('last',last);
+
