118c9caaf2770b2ac179f667252e345b44d082c9
chenxiaoxu <chenxiaoxu@daojia-inc.com>
2020-01-06 11:06:46 +0800

    init

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './store/test';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- learn-react/src/store/index.js
+++ learn-react/src/store/index.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducer";
 import { logger } from "redux-logger";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk";
 // import promise from "redux-promise";
-import promise from "../redux-promise";
+// import promise from "../redux-promise";
 
-
-// const store = createStore(reducer, applyMiddleware(thunk, logger));
-const store = createStore(reducer, applyMiddleware(promise, logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+// const store = createStore(reducer, applyMiddleware(promise, logger));
 export default store;
-
